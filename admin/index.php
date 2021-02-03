74f6e7331ea8515a8dfc609501f6ccc02d9a9cc2
gilangjanuar <gilangjanuar2305@gmail.com>
2021-02-03 09:50:42 +0700

    update UI

--- admin/index.php
+++ admin/index.php
@@ -1,55 +1,67 @@
 <?php include 'ProsesLoginAdmin.php'; ?>
 <html>
 
-  <head>
+<head>
     <title> SMKN 2 Kota Bandung </title>
     <link rel="shortcut icon" href="../img/Logo-SMKN2.png"/>
     <meta charset="utf-8">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <link href='../assets/css/bootstrap.css' rel='stylesheet' type='text/css'/>
-  </head>
-    <body>
-      <div class="col-md-12">
-  	    <div class="panel panel-info">
-  	      <div class="panel-heading">
-  	    <table border="0" width="1300" style="text-align:center">
-  	      <tr>
-  	        <td> <img src="../img/Logo-SMKN2.png" height="200" width="200"> </img> </td>
-  	        <td> <h1> SMK Negeri 2 Bandung </h1><br>
-  	        Jalan Ciliwung Nomor 4,Telepon 022-7234285, <br>
-  	        Faksimil 022-4231857, Website  www.smkn2bandung.sch.id <br>
-  	        email : humas@smkn2bandung.sch.id <br>
-  	        Kota Bandung - 40114 </td>
-  	        <td> <img src="../img/Logo3.PNG" height="200" width="200"> </img> </td>
-  	      </tr>
-  	    </table>
-  	  </div>
-      <div class="panel-body">
-        <form name="login" method="post" action="" class="form-horizontal">
-            <div class="input-group">
-              <span class="input-group-addon">
-                <i class="glyphicon glyphicon-user"> </i>
-              </span>
-                <input type="text" name="admin" placeholder="Masukan Id" class="form-control input-lg text-center" required >
-            </div>
-            <div class="input-group">
-              <span class="input-group-addon">
-                <i class="glyphicon glyphicon-lock"> </i>
-              </span>
-                <input type="password" name="pass" placeholder="Masukan password" class="form-control input-lg text-center" required>
+</head>
+<body>
+<div class="container">
+    <div class="panel panel-info" style="margin-top: 100px">
+        <div class="panel-heading">
+            <table border="0" width="100%" style="text-align:center">
+                <tr>
+                    <td><img src="../img/Logo-SMKN2.png" height="200" width="200"> </img></td>
+                    <td><h1> SMK Negeri 2 Bandung </h1><br>
+                        Jalan Ciliwung Nomor 4,Telepon 022-7234285, <br>
+                        Faksimil 022-4231857, Website www.smkn2bandung.sch.id <br>
+                        email : humas@smkn2bandung.sch.id <br>
+                        Kota Bandung - 40114
+                    </td>
+                    <td><img src="../img/Logo3.PNG" height="200" width="200"> </img></td>
+                </tr>
+            </table>
+        </div>
+        <div class="panel-body">
+            <form name="login" method="post" action="" class="">
+                <div class="form-group">
+                    <div class="input-group">
+                        <div class="input-group-addon">
+                            <i class="glyphicon glyphicon-user"> </i>
+                        </div>
+                        <input type="text" name="admin" id="admin" placeholder="Masukan ID Pengguna"
+                               class="form-control input-lg text-center"
+                               required>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="input-group">
+                        <div class="input-group-addon">
+                            <i class="glyphicon glyphicon-lock"> </i>
+                        </div>
+                        <input type="password" name="pass" placeholder="Masukan Kata Sandi Pengguna"
+                               class="form-control input-lg text-center" required>
+                    </div>
+                </div>
+        </div>
+        <div class="panel-footer">
+            <div class="row">
+                <div class="col-md-6">
+                    <button type="button" name="kembali" class="btn btn-success btn-lg btn-block"
+                            onclick="window.location = '../index.php'"><i class="glyphicon glyphicon-home"> </i> Kembali
+                    </button>
+                </div>
+                <div class="col-md-6">
+                    <button type="submit" name="masuk" class="btn btn-primary btn-lg btn-block"><i
+                                class="glyphicon glyphicon-log-in"> </i> Masuk
+                    </button>
+                </div>
             </div>
         </div>
-        <div class="panel-Footer">
-            <ul class="pager">
-              <li class="previous">
-              <button type="button" name="kembali"  class="btn btn-info btn-lg" onclick="window.location = '../index.php'"> <i class="glyphicon glyphicon-home">  </i> Kembali      </button>
-              </li>
-            <li class="next">
-                <button type="submit" name="masuk"  class="btn btn-info btn-lg">  <i class="glyphicon glyphicon-log-in"> </i>  Masuk   </button>
-            </li>
-        </div>
-            </div>
-      </div>
-    </body>
+    </div>
+</body>
 </html>
--- index.php
+++ index.php
@@ -1,39 +1,66 @@
 <html>
-  <head>
+<head>
     <title> SMKN 2 Kota Bandung </title>
     <link rel="shortcut icon" href="img/Logo-SMKN2.png"/>
     <meta charset="utf-8">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
     <link href='assets/css/bootstrap.css' rel='stylesheet' type='text/css'/>
-  </head>
-  <body>
-    <div class="col-md-12">
-	    <div class="panel panel-info">
-	      <div class="panel-heading">
-	    <table border="0" width="1300" style="text-align:center">
-	      <tr>
-	        <td> <img src="img/Logo-SMKN2.png" height="200" width="200"> </img> </td>
-	        <td> <h1> SMK Negeri 2 Bandung </h1><br>
-	        Jalan Ciliwung Nomor 4,Telepon 022-7234285, <br>
-	        Faksimil 022-4231857, Website  www.smkn2bandung.sch.id <br>
-	        email : humas@smkn2bandung.sch.id <br>
-	        Kota Bandung - 40114 </td>
-	        <td> <img src="img/Logo3.PNG" height="200" width="200"> </img> </td>
-	      </tr>
-	    </table>
-	  </div>
-    <div class="panel-body">
-      <form name="tombol" class="inner-login form-horizontal">
-        <a href="absen/TampilanKehadiran.php" class="btn btn-block btn-info btn-lg">
-            <span class="glyphicon glyphicon-list-alt"> </span> Kehadiran
-        </a>
-        <a href="../TugasAkhir/admin/index.php" class="btn btn-block btn-warning btn-lg">
-          <span class="glyphicon glyphicon-user"> </span> Login Admin
-        </a>
-      </form>
-      </div>
+</head>
+<body>
+<div class="container">
+    <div class="panel panel-info" style="margin-top: 100px">
+        <div class="panel-heading">
+            <table border="0" width="100%" style="text-align:center">
+                <tr>
+                    <td><img src="img/Logo-SMKN2.png" height="200" width="200"> </img></td>
+                    <td><h1> SMK Negeri 2 Bandung </h1><br>
+                        Jalan Ciliwung Nomor 4,Telepon 022-7234285, <br>
+                        Faksimil 022-4231857, Website www.smkn2bandung.sch.id <br>
+                        email : humas@smkn2bandung.sch.id <br>
+                        Kota Bandung - 40114
+                    </td>
+                    <td><img src="img/Logo3.PNG" height="200" width="200"> </img></td>
+                </tr>
+            </table>
+        </div>
+        <div class="panel-body">
+            <form name="tombol" class="inner-login form-horizontal">
+                <div class="row">
+                    <div class="col-md-6">
+                        <a href="absen/TampilanKehadiran.php">
+                            <div class="panel panel-primary">
+                                <div class="panel-heading">
+                                    <span class="glyphicon glyphicon-list-alt"> </span> Kehadiran
+                                </div>
+                                <div class="panel-body">
+                                    <p>
+                                        Menu Untuk siswa/i melakukan absen dengan memasukan NIS dari
+                                        setiap siswa tersebut
+                                    </p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-md-6">
+                        <a href="admin/index.php" class="">
+                            <div class="panel panel-success">
+                                <div class="panel-heading">
+                                    <span class="glyphicon glyphicon-user"> </span> Administrator
+                                </div>
+                                <div class="panel-body">
+                                    <p>
+                                        Menu Untuk admin / Karyawan Tata Usaha yang mengurus perihal
+                                        absensi para siswa/i
+                                    </p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </form>
+        </div>
     </div>
-  </div>
-  </body>
+</div>
+</body>
 </html>
